4fb1b2cd04152cc065e46b31cc07e768f09d611e
Austinjm121 <austinjm121@gmail.com>
2022-07-09 00:02:01 -0500

    Iteration 1

---
+++ assets/index.js
@@ -0,0 +1,29 @@
+var listOfCities =$("#city-list");
+var cities = [];
+var key = "NO KEY YET";
+
+//Format for day
+function FormatDay(date){
+    var date = new Date();
+    console.log(date);
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+    
+    var dayOutput = date.getFullYear() + '/' +
+        (month<10 ? '0' : '') + month + '/' +
+        (day<10 ? '0' : '') + day;
+    return dayOutput;
+}
+
+    
+  
+  
+    
+
+
+
+  
+
+
+
+
